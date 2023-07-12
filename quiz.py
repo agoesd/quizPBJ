bd338e165d56735e4a46f4cf1a8eaa7cc8600cef
agoesd <85086014+agoesd@users.noreply.github.com>
2023-07-12 20:49:43 +0700

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -2,20 +2,25 @@ import streamlit as st
 import pandas as pd
 import random
 
-# Load questions from a CSV file and randomize the options
+# Load questions from a CSV file
 def load_questions(url):
     df = pd.read_csv(url, delimiter=";")
     questions = []
     for _, row in df.iterrows():
         options = [row[i] for i in range(1, 5)]
-        random.shuffle(options)
         question = {
             "question": row[0],
             "options": options,
             "answer": row[5]
         }
         questions.append(question)
+    return questions
+
+# Randomize the order of questions and options
+def randomize_questions(questions):
     random.shuffle(questions)
+    for question in questions:
+        random.shuffle(question["options"])
     return questions
 
 # Calculate the total score
@@ -31,6 +36,7 @@ st.title("Quiz Time!")
 
 # Load the questions from the CSV file and randomize the options
 questions = load_questions("quiz_questions.csv")
+questions = randomize_questions(questions)
 
 # Maximum number of questions
 max_num_questions = len(questions)
@@ -46,13 +52,12 @@ if submitted_num_questions:
     st.session_state["quiz_started"] = True
     st.session_state["question_index"] = 0
     st.session_state["user_answers"] = [None] * num_questions
-    st.session_state["options"] = selected_questions[0]["options"]
 
 if st.session_state.get("quiz_started"):
     question = st.session_state["selected_questions"][st.session_state["question_index"]]
     st.header(f"Question #{st.session_state['question_index'] + 1}")
     st.write(question["question"])
-    selected_option = st.radio(f"Select an option for Question #{st.session_state['question_index'] + 1}:", question["options"], key=f"options_{st.session_state['question_index']}")
+    selected_option = st.selectbox(f"Select an option for Question #{st.session_state['question_index'] + 1}:", question["options"])
     st.session_state["user_answers"][st.session_state["question_index"]] = selected_option
 
     st.session_state["question_index"] += 1
