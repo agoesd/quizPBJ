aa970d04d44d4aa9efbe930711ab887549ad7582
agoesd <85086014+agoesd@users.noreply.github.com>
2023-07-13 15:58:25 +0700

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -55,14 +55,11 @@ if st.session_state.get("quiz_started"):
 
     if st.button("Next Question"):
         st.session_state["question_index"] += 1
-        if st.session_state["question_index"] >= num_questions:
-            st.session_state["quiz_started"] = False
-            st.session_state["score"] = sum(
-                4 if answer == question["answer"] else 0
-                for answer, question in zip(st.session_state["user_answers"], st.session_state["selected_questions"])
-            )
 
 if not st.session_state.get("quiz_started"):
     st.write("Quiz ended. Here's your score:")
-    score = st.session_state.get("score", 0)
+    score = sum(
+        4 if answer == question["answer"] else 0
+        for answer, question in zip(st.session_state["user_answers"], st.session_state["selected_questions"])
+    )
     st.success(f"Total Score: {score}")
